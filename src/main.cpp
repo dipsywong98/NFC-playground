215242e509a07c136540a5ed762e3a71f8d339ec
dipsywong98 <facebookarmyoffcal@gmail.com>
2018-02-10 16:09:27 +0800

    trying touch screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * main.cpp
  *
- * Author: 
+ * Author:
  * Copyright (c) 2014-2015 HKUST SmartCar Team
  * Refer to LICENSE for details
  */
@@ -13,7 +13,10 @@
 #include <libsc/led.h>
 #include <vector>
 
+#include "libbase/k60/flash.h"
+#include "libsc/k60/touchscreen_lcd.h"
 #include "libsc/k60/dk100.h"
+#include "libutil/touch_menu.h"
 
 namespace libbase
 {
@@ -36,109 +39,149 @@ using namespace libsc;
 using namespace libbase::k60;
 using libsc::k60::Dk100;
 using std::vector;
+using libsc::k60::TouchScreenLcd;
+using libutil::Touch_Menu;
 
-Led* pLed = nullptr;
-
-int success_time = 0;
-int recieve_time = 0;
-int write_time = 0;
-int read_time = 0;
-int data = 0;
-Dk100 *pNfc;
-
-uint8_t a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,know=0;
-int g_size = 0;
-
-vector<Byte> buffer;
-int destroy_time = 0, handle_time = 0;
-int send_time = 0;
-
-bool waiting_write = false;
-Byte sending_buffer[8];
-void SendWriteHelper(){
-	pNfc->SendBuffer(sending_buffer,8);
-}
-void SendWrite(Byte sector, Byte *to_write){
-	waiting_write = true;
-	Byte buf[8] = {0xAA, 0x06, 0x0A, sector};
-	memcpy(sending_buffer,buf,4);
-	memcpy(sending_buffer+4,to_write,4);
-	SendWriteHelper();
+namespace Sectors{
+const Byte
+kIdBalance = 0x04,
+kName = 0x05,
+kChecksum = 0x0D,
+kHistory = 0x0E,
+kCursor = 0xE0,
+kLastTap = 0xE1;
 }
 
-bool waiting_read = false;
-void SendReadHelper(){
-	pNfc->SendBuffer(sending_buffer,4);
-}
-void SendRead(Byte sector){
-	waiting_read = true;
-	Byte buf[4] = {0xAA, 0x02, 0x09, sector};
-	memcpy(sending_buffer,buf,4);
-	SendReadHelper();
-}
+Dk100 *pNfc = nullptr;
 
-void handler(const vector<Byte>& v){
-	handle_time++;
-	if(v.size()>0) a=v[0];
-	if(v.size()>1) b=v[1];
-	if(v.size()>2) c=v[2];
-	if(v.size()>3) d=v[3];
-	if(v.size()>4) e=v[4];
-	if(v.size()>5) f=v[5];
-	if(v.size()>6) g=v[6];
-	if(v.size()>7) h=v[7];
-	if(v.size()>8) i=v[8];
-	if(v.size()>9) j=v[9];
-	if(v.size()>10) k=v[10];
-	if(v.size()>11) l=v[11];
+Byte a=0,b=0,c=0,d=0,know=0;
+uint32_t buf_;
+int write_success_time = 0, read_success_time = 0;
 
-	//trigger when card tab
-	if(v[2] == 0x01){
-		send_time++;
-//		Byte buf[7] = {0xAA, 0x01, 0x15};
-//		pNfc->SendBuffer(buf,3);
-		Byte to_send[4] = {0x01,0x02,0x03,0x04};
-		SendWrite(0x06,to_send);
-	}
+uint16_t card_id = 0;
+int16_t balance = 0;
 
-	//trigger when write success
-	if(waiting_write && v[2] == 0xFE){
-		waiting_write = false;
-	}
+uint32_t last_tap = 0, checksum = 0;
 
-	//trigger when read success
-	if(v[2] == 0x90){
-		waiting_read = false;
-	}
+uint32_t A = 4219, B = 97729, C = 543217;
+uint8_t last_sector = 0x0E;
+
+inline uint32_t CalChecksum(uint16_t id, int16_t balance, uint32_t timestamp){
+	return (B+(A*((id+balance+timestamp)%C))%C)%C;
 }
 
-bool listener(const Byte* data, const size_t size){
-	know = a;	//AA
-	know = b;	//length
-	know = c;	//cmd
-	know = d;	//1
-	know = e;	//2
-	know = f;	//3
-	know = g;	//4
-	know = h;
-	know = i;
-	recieve_time++;
-	for(uint8_t i=0; i<size; i++){
-		buffer.push_back(data[i]);
-		if(buffer.size() > 1){
-			if(buffer.size() == buffer[1]+2){
-				handler(buffer);
-				buffer.clear();
-			} else if (buffer.size() > buffer[1]+2) {
-				destroy_time++;
-				buffer.clear();
-			}
-		}
-	}
+void OnWrite(const Byte& sector){
+	write_success_time++;
+}
+
+void OnRead(const Byte& sector, const Byte *data){
+	read_success_time++;
+	a=data[0];
+	b=data[1];
+	c=data[2];
+	d=data[3];
+}
+
+void WriteCard(){
+
+}
+
+bool ReadCard(){
+	//card id and balance
+	if(!pNfc->SendRead(0x04))return false;
+	buf_ = pNfc->GetData();
+	memcpy(&card_id, &buf_,2);
+	memcpy(&balance, &buf_+2,2);
+
+	//checksum
+	pNfc->SendRead(0x0D);
+	buf_ = pNfc->GetData();
+	memcpy(&checksum, &buf_, 4);
+
+	//last tap timestamp
+	buf_ = pNfc->SendRead(0xE1);
+	memcpy(&last_tap, &buf_, 4);
+
+	//return if this card is valid
 	return true;
 }
 
-int main(void)
+bool FormatCard(uint16_t id, int16_t balance){
+	Byte temp[4];
+	memset(temp, 0, 4);
+
+	for(uint8_t i = 0x04; i<=0xE1; i++){
+		if(pNfc->SendWrite(i,temp))return false;
+	}
+
+	memcpy(temp,&id,2);
+	memcpy(temp+2,&balance,2);
+	pNfc->SendWrite(0x04,temp);
+
+	uint32_t time = 0;
+	memcpy(temp, &time, 4);
+	pNfc->SendWrite(Sectors::kLastTap,temp);
+
+	uint32_t cs = CalChecksum(id,balance,time);
+	memcpy(temp, &cs, 4);
+	pNfc->SendWrite(0x0D, temp);
+
+	uint8_t cursor = 0;
+	memset(temp, 0, 4);
+	memcpy(temp, &cursor, 1);
+	pNfc->SendWrite(Sectors::kCursor, temp);
+	return true;
+}
+
+int yo = 0;
+
+int main(){
+	System::Init();
+	Led::Config led_config;
+//	led_config.id = 2;
+//	Led led2(led_config);
+//	led_config.id = 3;
+//	Led led3(led_config);
+
+	bool show = false;
+
+	TouchScreenLcd lcd([&](Gpi*,TouchScreenLcd* pLcd){
+		yo++;
+		show = true;
+	});
+//	Flash::Config flash_config;
+//	Flash flash(flash_config);
+////	lcd.DisplayOn();
+////	lcd.ShowNum(0,0,1234,4,48);
+////	lcd.DrawCircle(50,50,50);
+//	Touch_Menu menu(&lcd,&flash);
+//	uint8_t h=0;
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.AddItem("hi",&h,&menu.main_menu);
+//	menu.EnterMenu(&menu.main_menu,0,0,480,800,48);
+	while(1){
+//			led2.Switch();
+		lcd.ShowNum(0,0,yo,4,48);
+		lcd.ShowNum(0,50,lcd.touch_status,1,48);
+		lcd.ShowNum(0,100,System::Time(),4,48);
+		lcd.ShowNum(0,150,lcd.Scan(0),1,48);
+	}
+}
+
+int main_(void)
 {
 	System::Init();
 
@@ -158,12 +201,12 @@ int main(void)
 	led2.SetEnable(1);
 	led3.SetEnable(1);
 
-	pLed = &led1;
-
     Dk100::Config config;
     config.id = 0;
     config.baud_rate = libbase::k60::Uart::Config::BaudRate::k9600;
-    config.rx_isr = listener;
+    config.rx_isr = [&](const Byte* buff, const size_t& size){
+    	return pNfc->Listener(buff,size);
+    };
     Dk100 nfc(config);
     pNfc = &nfc;
 
@@ -178,22 +221,24 @@ int main(void)
 
 //    SendRead(0x06);
     Byte to_[4] = {0x01,0x02,0x03,0x04};
-    SendWrite(0x06,to_);
-	while (true){
-	    led1.SetEnable(!waiting_write);
-	    led2.SetEnable(!waiting_read);
-	    if(System::Time()%10){
 
-			if(waiting_write){
-				SendWriteHelper();
-			}
-			else if(waiting_read==false){
-				SendRead(0x06);
-			}
-			if(waiting_read){
-				SendReadHelper();
-			}
-	    }
+    nfc.SetWriteSuccessHandler(OnWrite);
+    nfc.SetReadSuccessHandler(OnRead);
+
+//    nfc.SendWrite(0x06,to_);
+//    nfc.SendRead(0x06);
+
+    if(ReadCard()){
+
+    }
+
+	while (true){
+		led0.Switch();
+		System::DelayMs(250);
+//		know=a;
+//		know=b;
+//		know=c;
+//		know=d;
 	}
 
 	return 0;
